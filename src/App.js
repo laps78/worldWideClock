ba099e9ea4044931b27b188c5a4cabac8f116e74
laps78 <9254384@mail.ru>
2023-02-23 16:36:12 +0300

    index.html title updated. Basic App components structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Form from './components/Form';
+import { useState } from 'react';
 
 function App() {
+  const [clocks, setClocks] = useState([]);
+  const ClockTable = ({ children }) => <div className='ClockTable'>{children}</div>;
+  const changeState = (newClocks) => setClocks(newClocks);
   return (
     <div className="App">
-      <Form />
+      <Form changeState={ changeState }/>
+      <ClockTable children={clocks} />
     </div>
   );
 }
